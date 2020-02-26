ba0c272415cabf195e52b42848868ebb4121ac5e
junhwan <ujunhwan@gmail.com>
2020-02-26 23:36:23 +0900

    11022번 A+B -8

---
+++ BAEKJOON/11022.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int num1;
+    int num2;
+    int T;
+
+    cin >> T;
+    for(int i=0;i<T;i++)
+    {   
+        cin >> num1 >> num2;
+        cout << "Case #"<< i+1 << ": " << num1 << " + " << num2 << " = " << num1+num2 <<'\n';
+    }
+}
