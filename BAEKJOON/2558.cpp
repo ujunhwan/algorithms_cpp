43d35a154f3bcae9da8ef39f02d7866817336ca2
junhwan <ujunhwan@gmail.com>
2020-02-26 22:40:12 +0900

    2588번 A+B -2

---
+++ BAEKJOON/2558.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int num1,num2;
+
+    cin >> num1 >> num2;
+    cout << num1+num2 << '\n';
+}
