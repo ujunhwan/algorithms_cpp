823e2bc7414f546a76bba6417e65c9772d948d10
junhwan <ujunhwan@gmail.com>
2020-03-09 17:33:26 +0900

    2446번 별 찍기 - 9

---
+++ BAEKJOON/2446.cpp
@@ -0,0 +1,22 @@
+// 2446번 별 찍기 - 9
+#include <bits/stdc++.h>
+int main()
+{
+    int N; scanf("%d",&N);
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=1;j<=i-1;j++)
+            printf(" ");
+        for(int k=1;k<=2*N-2*i+1;k++)
+            printf("*");
+        printf("\n");
+    }
+    for(int i=1;i<N;i++)
+    {
+        for(int j=1;j<=N-i-1;j++)
+            printf(" ");
+        for(int k=1;k<=2*i+1;k++)
+            printf("*");
+        printf("\n");
+    }
+}
