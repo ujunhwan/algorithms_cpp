3bfbe5c18e7d9662dcf0724ba232d99cee3c3660
junhwan <ujunhwan@gmail.com>
2020-03-10 18:22:17 +0900

    1929번 소수 구하기 (에라토스테네스의 체)

--- BAEKJOON/1929.cpp
+++ BAEKJOON/1929.cpp
@@ -1,17 +1,37 @@
 // 1929번 소수 구하기
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <algorithm>
 using namespace std;
-bool isPrime(int n)
-{
-    if(n < 2) return false;
-    for(int i=2;i*i<=n;i++)
-        if(n % i == 0) return false;
-    return true;
-}
+static const int bufSize = 1 << 20;
+bitset<bufSize> prime;
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    string ans = "";
     int M,N; cin >> M >> N;
-    for(int i=M;i<=N;i++)
-        if(isPrime(i))
-            cout << i << '\n';
+    if(M <= 2 && N >= 2)
+        ans += "2\n";
+    for(int i=3;i<=N;i += 2) // 2의 배수는 어차피 상관x
+    {
+        if(!prime.test(i))
+        {
+            for(int j=i+i;j<=N;j += i)
+                prime.set(j);
+            if(M <= i && N >= i)
+            {
+                ans += to_string(i);
+                ans += '\n';
+            }
+            if(ans.length() >= 1 << 19)
+            {
+                cout << ans << '\n';
+                ans = "";
+            }
+        }
+    }
+    cout << ans << '\n';
+    return 0;
 }
