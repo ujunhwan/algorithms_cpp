25cc9b3ac5bd7c1f19c6a6d6cc77ea5af6fa2a7c
junhwan <ujunhwan@gmail.com>
2020-03-11 16:24:52 +0900

    17087번 숨바꼭질 6

--- BAEKJOON/17087.cpp
+++ BAEKJOON/17087.cpp
@@ -4,26 +4,21 @@
 using namespace std;
 int GCD(int a, int b)
 {
-    while(b != 0)
-    {
-        int r = a % b;
-        a = b;
-        b = r;
-    }
-    return a;
+    return b ? GCD(b,a%b) : a;
 }
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
     int N,S; cin >> N >> S;
     int A[N];
+    int ans = 0;
     for(int i=0;i<N;i++)
     {
         cin >> A[i];
         A[i] = abs(S - A[i]);
-    }
-    long long ans = A[0];
-    for(int i=1;i<N;i++)
         ans = GCD(ans,A[i]);
+    }
     cout << ans;
     return 0;
 }
