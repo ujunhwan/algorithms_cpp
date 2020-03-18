57477d0e26c39b30428c83c239a3e640b1adc3cc
junhwan <ujunhwan@gmail.com>
2020-03-19 02:04:17 +0900

    10869번 사칙연산

---
+++ BAEKJOON/10869.cpp
@@ -0,0 +1,12 @@
+// 10869번 사칙연산
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b; cin >> a >> b;
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+    cout << a / b << '\n';
+    cout << a % b << '\n';
+}
