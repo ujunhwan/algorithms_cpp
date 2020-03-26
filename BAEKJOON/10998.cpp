5ea55e0b6e90c7d6e83e8e327326a4ec43c47f2e
junhwan <ujunhwan@gmail.com>
2020-03-26 20:56:15 +0900

    10998번 AxB

---
+++ BAEKJOON/10998.cpp
@@ -0,0 +1,9 @@
+// 10998번 AxB
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    int a,b; cin >> a >> b;
+    cout << a*b << '\n';
+}
