603aa9ebbaa0e94c31a60eab882255bdc0bc02a0
junhwan <ujunhwan@gmail.com>
2020-04-21 21:22:18 +0900

    17103번 골드바흐 파티션

--- BAEKJOON/17103.cpp
+++ BAEKJOON/17103.cpp
@@ -1,36 +1,29 @@
 // 17103번 골드바흐 파티션
-#include <iostream>
+#include <cstdio>
 #include <bitset>
 #include <algorithm>
 using namespace std;
 static const int bufSize = 1 << 20;
 bitset <bufSize> prime;
-bool isPrime(int n)
-{
+inline bool isPrime(int n) {
+    if (n > 2 && n % 2 == 0) return false;
     return prime.test(n) ? false : true;
 }
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
-    for(int i=3;i<=1000;i+=2)
-    {
+int main() {
+    for(int i = 3; i <= 1000; i += 2) {
         if(!prime.test(i))
-            for(int j=i*i;j<=bufSize;j+=i)
+            for(int j = i + i; j <= bufSize; j += i)
                 prime.set(j);
     }
-    int T; cin >> T;
-    while(T--)
-    {
-        int N; cin >> N;
-        int cnt = 0;
-        for(int i=3;i<=N/2;i += 2)
-        {
-            int a = N - i;
-            int b = i;
-            if(isPrime(a) && isPrime(b))
-                cnt += 1;
+    int t; scanf("%d", &t);
+    while(t--) {
+        int n, cnt = 0; scanf("%d", &n);
+        for(int i = 2; i <= n / 2; i++) {
+            int a = n - i, b = i;
+            if(isPrime(a) && isPrime(b)) cnt++;
+            if (i == 2) continue; i++;
         }
-        cout << cnt << '\n';
+        printf("%d\n", cnt);
     }
+    return 0;
 }
